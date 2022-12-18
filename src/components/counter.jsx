1ed88adc72ab621285ce91bea3140189b5eae541
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2022-12-18 14:30:13 +0100

    building a count app

---
+++ src/components/counter.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count:  0
+    }
+    render() { 
+        return (
+            <React.Fragment>
+                <span className='badge bg-primary m-2'>{this.formatCounter()}</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
+            </React.Fragment>
+        );
+    }
+
+    formatCounter = () => {
+        const { count } = this.state
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
